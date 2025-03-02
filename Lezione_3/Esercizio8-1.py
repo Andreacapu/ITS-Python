379fe59114011ff5b29b47f44f8bef3716c6315c
Andreacapu <capuani.andrea@gmail.com>
2025-03-02 19:07:01 +0100

    Add files via upload

---
+++ Lezione_3/Esercizio8-1.py
@@ -0,0 +1,6 @@
+#inizializzazione della definizione del messaggio
+def display_messagge ():
+    message = "le funzioni sono estremamente utili e mi rendono la vita più facile"
+    print(message)
+#comando per mostrare il messaggio
+display_messagge()
