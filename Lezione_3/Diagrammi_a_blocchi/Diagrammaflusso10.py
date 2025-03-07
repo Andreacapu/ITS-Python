a08c751040d209db8c293200c46f5267bbb69d92
Andreacapu <capuani.andrea@gmail.com>
2025-03-07 22:02:45 +0100

    Add files via upload

--- Lezione_3/Diagrammi_a_blocchi/Diagrammaflusso10.py
+++ Lezione_3/Diagrammi_a_blocchi/Diagrammaflusso10.py
@@ -1,13 +1,12 @@
-media = int(input("inserire la media: "))
-reddito  = int(input("inserire il reddito: "))
+#settaggio condizioni
+reddito = int(input("inserire reddito: "))
+media = int(input("inserire media: "))
 
-match media:
-    case _ if media >= 27:
-        match reddito:
-            case _ if reddito < 20000:
-                print("borsa di studio assegnata")
-
-            case _:
-                print("borsa di studio non assegnata, media insufficente e/o reddito elevato")
-
-
+#settaggio match statement (il "_" serve a fare il match con ogni valore, con l'if che si attiva solo se la condione sia vera)
+match reddito:
+        case _ if reddito < 20000:
+            match media:
+                case _ if media > 27:
+                    print("borsa di studio assegnata")
+                case _:
+                      print("borsa di studio non assegnata: media inferiore e/o reddito elevato")
